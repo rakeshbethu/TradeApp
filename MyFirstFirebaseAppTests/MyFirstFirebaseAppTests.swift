c596d558443b89438c344730d664ca2b81b13a1f
Rakesh Bethu <rakeshbethu@gmail.com>
2018-04-19 17:08:07 -0400

    Initial Commit

---
+++ MyFirstFirebaseAppTests/MyFirstFirebaseAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  MyFirstFirebaseAppTests.swift
+//  MyFirstFirebaseAppTests
+//
+//  Created by Rakesh Bethu on 4/19/18.
+//  Copyright © 2018 Rakesh Bethu. All rights reserved.
+//
+
+import XCTest
+@testable import MyFirstFirebaseApp
+
+class MyFirstFirebaseAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
